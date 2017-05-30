52ae4c640047c29fb90bc7c21b23e89f8cfbf5fe
Cecilia A <Cecilia A>
2017-05-30 18:09:23 -0300

    Create description, title, image, duration and item elements and insert them into result id

--- js/custom.js
+++ js/custom.js
@@ -4,17 +4,32 @@ function searchCartoon() {
 	if (this.readyState == 4 && this.status == 200) {
 		var response = JSON.parse(this.responseText);
 		var i;
+		var counter = 0;
 		for(i = 0; i < response.length; i++) {
 			var movie = response[i];
 			var category = movie.category;
 			if(category == "cartoon") {
+				counter = counter + 1;
+				var result = document.getElementById("result");
+				var $item = document.createElement("li");
 				var $category = document.createElement("i");
-				var $title = document.createElement("h2");
-				var box = document.getElementById('box');
+				var $title = document.createElement("h3");
+				var $duration = document.createElement("p");
+				var $image = document.createElement("img");
+				var $description = document.createElement("div");
+				$description.setAttribute("class", "description");
 				$category.innerHTML = category;
 				$title.innerHTML = movie.film;
-				box.appendChild($category);
-				box.appendChild($title);
+				$duration.innerHTML = "Duration: " + movie.duration;
+				$image.setAttribute("src", movie.image.url);
+				$image.setAttribute("alt", movie.image.alt);
+				result.appendChild($item);
+				$description.appendChild($category);
+				$description.appendChild($title);
+				$description.appendChild($duration);
+				$item.appendChild($image);
+				$item.appendChild($description);
+				console.log(counter);
 			}
 		}
 	}
