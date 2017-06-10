5ec34601ebb755222198290416ba661701704e8c
Cecilia A <Cecilia A>
2017-06-10 13:18:56 -0300

    Erases the li structure and replace it by a js DOM creation

--- js/custom.js
+++ js/custom.js
@@ -5,41 +5,40 @@ function searchMovie(movieCategory) {
 			var response = JSON.parse(this.responseText);
 			var i;
 			var counter = 0;
+			var filmList = document.getElementById("filmList");
+			//erases any possible data inside the filmList element
+			filmList.innerHTML = "";				
+
 			for(i = 0; i < response.length; i++) {
 				var movie = response[i];
 				var category = movie.category;
 
-				//select the DOM elements
-				var result = document.getElementById('items');				
-				var $item = document.querySelector("#items li");
-				var $title = document.querySelector("#items h3");
-				var $category = document.querySelector("#items i");					
-				var $duration = document.querySelector("#items p");
-				var $image = document.querySelector("#items img");
-				var $description = document.querySelector("#items div");
+				//creates the DOM elements			
+				var $item = document.createElement("li");
+				var $title = document.createElement("h3");
+				var $category = document.createElement("i");		
+				var $duration = document.createElement("p");
+				var $image = document.createElement("img");
+				var $description = document.createElement("div");			
 
 				if(category == movieCategory) {
 					//counter = counter + 1;
-					var newItem = $item.cloneNode(true);
-					result.appendChild(newItem);
-					$item.removeAttribute("id", "templateItem");
-					document.getElementById("templateItem").style.display = "none";
 
-					$category.innerHTML = category;
+					//inserts the information inside the created elements
 					$title.innerHTML = movie.film;
 					$duration.innerHTML = "Duration: " + movie.duration;
+					$category.innerHTML = category;
 					$image.setAttribute("src", movie.image.url);
 					$image.setAttribute("alt", movie.image.alt);
-					result.style.display = "block";
-					/*console.log(counter);
-					if(movieCategory == "cartoon") {
-						result.setAttribute("class", "cartoon");
-						result.removeAttribute("class", "adventure");
-					}
-					if(movieCategory == "adventure") {
-						result.setAttribute("class", "adventure");
-						result.removeAttribute("class", "cartoon");
-					}*/
+					$description.setAttribute("class", "description");
+
+					//appends the created elements into de DOM
+					$description.appendChild($title);
+					$description.appendChild($category);
+					$description.appendChild($duration);
+					$item.appendChild($image);
+					$item.appendChild($description);
+					filmList.appendChild($item);					
 				}
 			}
 		}
@@ -49,6 +48,4 @@ function searchMovie(movieCategory) {
 };
 
 //OBJETIVES
-
-// clear the previous results when clicking on a new button
 // cheate a counter a show the number of results next to: "We found x results for x"
